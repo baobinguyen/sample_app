6bd4bd705e6acd9025414de2bce464794262922d
baobinguyen <nguyentranngocbao1997@gmial.com>
2018-11-27 15:42:11 +0700

    Rails-flavored Ruby

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,11 +1,21 @@
 require "test_helper"
-class PagesControllerTest < ActionDispatch::IntegrationTest
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get static_pages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
+
   test "should get help" do
-    get pages_help_url
+    get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
