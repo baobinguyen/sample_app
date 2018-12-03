7469ac9ae434a2b6be1bea2f1cccbcb6adc36e15
baobinguyen <nguyentranngocbao1997@gmial.com>
2018-12-03 11:21:26 +0700

    chapter 08

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -17,7 +17,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-  
   test "should get contact" do
     get contact_path
     assert_response :success
