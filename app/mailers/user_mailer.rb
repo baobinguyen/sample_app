c75294205eff68f93ce6b159a9cd0112c23c5c35
baobinguyen <nguyentranngocbao1997@gmial.com>
2018-12-09 17:09:34 +0700

    chapter 12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: I18n.t("mailers.user.account_activation")
   end
 
-  def password_reset
-    @greeting = I18n.t("mailers.user.hi")
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: I18n.t("mailers.user.password_reset")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,24 @@ class ActiveSupport::TestCase
                                         password: password,
                                         remember_me: remember_me}}
   end
+
+  def check_valid user
+    patch password_reset_path(user.reset_token),
+      params: {email: user.email,
+               user: {password: "foobaz",
+                      password_confirmation: "barquux"}}
+    assert_select "div#error_explanation"
+    patch password_reset_path(user.reset_token),
+      params: {email: user.email,
+               user: {password: "",
+                      password_confirmation: ""}}
+    assert_select "div#error_explanation"
+    patch password_reset_path(user.reset_token),
+      params: {email: user.email,
+               user: {password: "foobaz",
+                      password_confirmation: "foobaz"}}
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to user
+  end
 end
